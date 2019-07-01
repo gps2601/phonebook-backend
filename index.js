9d4b4bc06dc4c3830b4c6fd8675ed61921730717
Geoff Smith <smith260194@gmail.com>
2019-07-01 20:38:13 +0100

    Getting ready for deploy

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint);
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`Server running on the port ${port}`);
