8dd31e50312e0f17c04b4930f2bb3ea6f8ca495c
Geoff Smith <smith260194@gmail.com>
2019-07-01 20:52:39 +0100

    Adding frontend

---
+++ build/precache-manifest.5e2e2ecf447ac6644b2d6ca37f7b4f7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f4ef56090af047b8786079551dac93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d910887133c47f3625a",
+    "url": "/static/css/main.8da994cd.chunk.css"
+  },
+  {
+    "revision": "d9474e5d60507ff06ad6",
+    "url": "/static/js/2.f3573713.chunk.js"
+  },
+  {
+    "revision": "8d910887133c47f3625a",
+    "url": "/static/js/main.cbbed590.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
