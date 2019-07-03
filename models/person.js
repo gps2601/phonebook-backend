8f3489fb3f2cf748f45910883fef9ebc6423907e
Geoff Smith <smith260194@gmail.com>
2019-07-03 15:01:39 +0100

    Using dotenv for env variables

--- models/person.js
+++ models/person.js
@@ -1,21 +1,14 @@
 const mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1);
-}
-
-const password = process.argv[2];
-
 mongoose.connect(
-    `mongodb+srv://gps2601:${password}@fieldnote-3fejg.mongodb.net/phonebook?retryWrites=true&w=majority`
+    process.env.MONGO_DB_URL
     , {useNewUrlParser: true}
 );
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: {type: String, unique: true},
+    name: {type: String, minlength: 3},
+    number: {type: String, minlength: 8, unique: true},
 });
 
 personSchema.set('toJSON', {
